e65dc9b86a7c873e1f749317c9e8728486c76977
melmut66 <124189787+melmut66@users.noreply.github.com>
2023-03-15 11:15:35 +0100

    Add files via upload

--- C00/ex07/ft_putnbr.c
+++ C00/ex07/ft_putnbr.c
@@ -5,14 +5,43 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mcreus <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/04 15:06:36 by mcreus            #+#    #+#             */
-/*   Updated: 2022/12/04 17:22:55 by mcreus           ###   ########.fr       */
+/*   Created: 2023/03/15 10:50:10 by mcreus            #+#    #+#             */
+/*   Updated: 2023/03/15 11:13:39 by mcreus           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
+void	ft_putchar(char c)
+{
+	write (1, &c, 1);
+}
+
 void	ft_putnbr(int nb)
 {
-	
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		nb = -nb;
+	}
+	if (nb >= 10)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	else
+		ft_putchar(nb + '0');
+}
+
+int	main()
+{
+	ft_putnbr(42);
+	ft_putchar ('\n');
+	ft_putnbr(-56165561);
+	ft_putchar('\n');
+	ft_putnbr(1564);
+	ft_putchar('\n');
+	ft_putnbr(8);
+	ft_putchar('\n');
+	ft_putnbr(0);
 }
